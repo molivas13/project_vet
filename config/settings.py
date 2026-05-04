e028593ec039e38c37040af6b3043e50165a99a7
Mariee13 <107163803+Mariee13@users.noreply.github.com>
2026-05-03 23:54:55 -0600

    diseño del navbar

--- config/settings.py
+++ config/settings.py
@@ -116,4 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 AUTH_USER_MODEL = 'core.User'
